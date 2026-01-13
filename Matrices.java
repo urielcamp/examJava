0fdfa759bfbadaa41062db5c7e3e8f262a7a38e4
Uriel Campaña <urielcamp@gmail.com>
2026-01-13 15:52:21 -0300

    ejercicios de examen matrices

--- Matrices.java
+++ Matrices.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Matrices{
     public static void main(String[] args) {
 
@@ -139,10 +141,167 @@ public class Matrices{
             System.out.println();
         }
 
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("EJER 6");
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        //Suma de todos los elementos de una matriz 3x3.
+        //Muestra el resultado final.
+
+        int[][] mSeis = {
+            {1, 1, 1},
+            {1, 1, 1},
+            {1, 1, 1}
+        };
+
+        int s = 0;
+
+        for(int i = 0; i < mSeis.length; i++){
+            for(int j = 0; j < mSeis[i].length; j++){
+                s += mSeis[i][j];
+            }
+        }
+
+        System.out.println(s);
+
+
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("EJER 7");
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        //Promedio de todos los elementos de una matriz 3x3.
+        //Devuelve un número decimal.
+
+        double[][] mSiete = {
+            {1.0, 2.0, 3.0},
+            {2.0, 3.0, 4.0},
+            {3.0, 4.0, 5.0}
+        };
+
+        double su = 0.0;
+        double r = 0.0;
+        int coun = 0;
+        for(int i = 0; i < mSiete.length; i++){
+            for(int j = 0; j < mSiete[i].length; j++){
+                su += mSiete[i][j];
+                coun++;
+            }
+        }
+
+        r = su / coun;
+
+        System.out.println(r);
+
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("EJER 8");
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        //Número mayor en una matriz 3x3.
+        //Encuentra y muestra el valor más grande.
+
+        int[][] mOcho = {
+            {1223, 2, 3},
+            {23, 4, 5},
+            {1, 9, 456768}
+        };
+
+        int a = mOcho[0][0];
+
+        for(int i = 0; i < mOcho.length; i++){
+            for(int j = 0; j < mOcho[i].length; j++){
+                if (a < mOcho[i][j]) {
+                    a = mOcho[i][j];
+                }
+            }
+        }
+
+        System.out.println(a);
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("EJER 9");
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        //Número menor en una matriz 3x3.
+        //Encuentra y muestra el valor más pequeño.
+
+        int[][] mNueve = {
+            {0, 21, -9},
+            {1, 23, 4},
+            {9, 90, 89}
+        };
+
+        int au = mNueve[0][0];
+
+        for(int i = 0; i < mNueve.length; i++){
+            for(int j = 0; j < mNueve[i].length; j++){
+                if (au > mNueve[i][j]) {
+                    au = mNueve[i][j];
+                }
+            }
+        }
+
+        System.out.println(au);
+
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("EJER 10");
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        //Buscar el mayor y menor y colocarlos en un array
+
+
+        int[][] mDiez = {
+            {12, 2 , 3},
+            {-9, 9, 2}
+        };
+
+        int max =  mDiez[0][0];
+
+        int min = mDiez[0][0];
+
+        int[] aux = new int[2];
+
+        for(int i = 0; i < mDiez.length; i++){
+            for(int j = 0; j < mDiez[i].length; j++){
+                if (max < mDiez[i][j]) {
+                    max = mDiez[i][j];
+                }
+
+                if(min > mDiez[i][j]){
+                    min = mDiez[i][j];
+                }
+            }
+        }
+
+
+        aux[0] = min;
+        aux[1] = max;
+
+        System.out.println(Arrays.toString(aux));
 
 
 
     }
-
-
 }
